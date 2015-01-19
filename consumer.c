9725290b84e2604ae193e4055328d11ae07125fb
Rick Richardson <rick.richardson@gmail.com>
2015-01-19 15:45:50 -0500

    styling for phil

--- consumer.c
+++ consumer.c
@@ -17,97 +17,97 @@
 
 int main(int argc, char const *const *argv)
 {
-  char const *hostname;
-  int port, status, iters;
-  char const *exchange;
-  char const *bindingkey;
-  amqp_socket_t *socket = NULL;
-  amqp_connection_state_t conn;
+    char const *hostname;
+    int port, status, iters;
+    char const *exchange;
+    char const *bindingkey;
+    amqp_socket_t *socket = NULL;
+    amqp_connection_state_t conn;
 
-  amqp_bytes_t queuename;
+    amqp_bytes_t queuename;
 
-  if (argc < 4) {
-    fprintf(stderr, "Usage: amqp_listen host port iters\n");
-    return 1;
-  }
-
-  hostname = argv[1];
-  port = atoi(argv[2]);
-  iters = atoi(argv[3]);
-
-  conn = amqp_new_connection();
-
-  socket = amqp_tcp_socket_new(conn);
-  if (!socket) {
-    die("creating TCP socket");
-  }
-
-  status = amqp_socket_open(socket, hostname, port);
-  if (status) {
-    die("opening TCP socket");
-  }
-
-  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
-                    "Logging in");
-  amqp_channel_open(conn, 1);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
-
-  {
-    amqp_queue_declare_ok_t *r = amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1,
-                                 amqp_empty_table);
-    die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
-    queuename = amqp_bytes_malloc_dup(r->queue);
-    if (queuename.bytes == NULL) {
-      fprintf(stderr, "Out of memory while copying queue name");
-      return 1;
+    if (argc < 4) {
+        fprintf(stderr, "Usage: amqp_listen host port iters\n");
+        return 1;
     }
-  }
 
-  amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes("amq.direct"), amqp_cstring_bytes("test queue"), amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
+    hostname = argv[1];
+    port = atoi(argv[2]);
+    iters = atoi(argv[3]);
 
-  amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
+    conn = amqp_new_connection();
 
-  int count = 0;
-  while (count++ < iters) {
-      amqp_rpc_reply_t res;
-      amqp_envelope_t envelope;
+    socket = amqp_tcp_socket_new(conn);
+    if (!socket) {
+        die("creating TCP socket");
+    }
 
-      amqp_maybe_release_buffers(conn);
+    status = amqp_socket_open(socket, hostname, port);
+    if (status) {
+        die("opening TCP socket");
+    }
 
-      res = amqp_consume_message(conn, &envelope, NULL, 0);
+    die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
+                      "Logging in");
+    amqp_channel_open(conn, 1);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
 
-      if (AMQP_RESPONSE_NORMAL != res.reply_type) {
-        break;
-      }
+    {
+        amqp_queue_declare_ok_t *r = amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1,
+                                                        amqp_empty_table);
+        die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
+        queuename = amqp_bytes_malloc_dup(r->queue);
+        if (queuename.bytes == NULL) {
+            fprintf(stderr, "Out of memory while copying queue name");
+            return 1;
+        }
+    }
 
-      printf("--%d-- Delivery %u, exchange %.*s routingkey %.*s\n",
-             count,
-             (unsigned) envelope.delivery_tag,
-             (int) envelope.exchange.len, (char *) envelope.exchange.bytes,
-             (int) envelope.routing_key.len, (char *) envelope.routing_key.bytes);
+    amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes("amq.direct"), amqp_cstring_bytes("test queue"), amqp_empty_table);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
 
-      if (envelope.message.properties._flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
-        printf("Content-type: %.*s\n",
-               (int) envelope.message.properties.content_type.len,
-               (char *) envelope.message.properties.content_type.bytes);
-      }
-      printf("----\n");
+    amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
 
-      amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
-      struct ipope_drop_copy dc;
-      int sz = erl_to_dc(envelope.message.body.bytes, &dc);
-      printf("Received drop copy message of %d bytes in a payload of %lu bytes\n", sz, envelope.message.body.len);
-      amqp_destroy_envelope(&envelope);
-  }
-  printf("Done receiving messages\n");
+    int count = 0;
+    while (count++ < iters) {
+        amqp_rpc_reply_t res;
+        amqp_envelope_t envelope;
+
+        amqp_maybe_release_buffers(conn);
+
+        res = amqp_consume_message(conn, &envelope, NULL, 0);
+
+        if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+            break;
+        }
+
+        printf("--%d-- Delivery %u, exchange %.*s routingkey %.*s\n",
+               count,
+               (unsigned) envelope.delivery_tag,
+               (int) envelope.exchange.len, (char *) envelope.exchange.bytes,
+               (int) envelope.routing_key.len, (char *) envelope.routing_key.bytes);
+
+        if (envelope.message.properties._flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
+            printf("Content-type: %.*s\n",
+                   (int) envelope.message.properties.content_type.len,
+                   (char *) envelope.message.properties.content_type.bytes);
+        }
+        printf("----\n");
+
+        amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
+        struct ipope_drop_copy dc;
+        int sz = erl_to_dc(envelope.message.body.bytes, &dc);
+        printf("Received drop copy message of %d bytes in a payload of %lu bytes\n", sz, envelope.message.body.len);
+        amqp_destroy_envelope(&envelope);
+    }
+    printf("Done receiving messages\n");
 
 
-  die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
-  die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
-  die_on_error(amqp_destroy_connection(conn), "Ending connection");
+    die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+    die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+    die_on_error(amqp_destroy_connection(conn), "Ending connection");
 
-  return 0;
+    return 0;
 }
 
--- producer.c
+++ producer.c
@@ -15,18 +15,18 @@
 
 static int construct_dc(char** buffer) {
     struct pope_header hdr = { .msg_type = 0,
-                               .msg_length = sizeof(struct pope_header) + sizeof(struct ipope_drop_copy),
-                               .originator = 12222,
-                               .seq_num = 12345 };
+        .msg_length = sizeof(struct pope_header) + sizeof(struct ipope_drop_copy),
+        .originator = 12222,
+        .seq_num = 12345 };
 
     struct ipope_drop_copy dc = { .symbol_id = 8888,
-                                  .passive_party = 54321,
-                                  .aggressor_party = 12345,
-                                  .exec_size = 100000,
-                                  .exec_price = 9999,
-                                  .passive_order_id = 888885555544,
-                                  .passive_order_id = 444485555544,
-                                  .exec_ts = 269945769778649 };
+        .passive_party = 54321,
+        .aggressor_party = 12345,
+        .exec_size = 100000,
+        .exec_price = 9999,
+        .passive_order_id = 888885555544,
+        .passive_order_id = 444485555544,
+        .exec_ts = 269945769778649 };
 
     struct ipope_drop_copy odc = { 0 };
 
@@ -45,104 +45,104 @@ static void send_batch(amqp_connection_state_t conn,
                        int rate_limit,
                        int message_count)
 {
-  uint64_t start_time = now_microseconds();
-  int i;
-  int sent = 0;
-  int previous_sent = 0;
-  uint64_t previous_report_time = start_time;
-  uint64_t next_summary_time = start_time + SUMMARY_EVERY_US;
+    uint64_t start_time = now_microseconds();
+    int i;
+    int sent = 0;
+    int previous_sent = 0;
+    uint64_t previous_report_time = start_time;
+    uint64_t next_summary_time = start_time + SUMMARY_EVERY_US;
 
-  char* message = NULL;
-  char sz = construct_dc(&message);
+    char* message = NULL;
+    char sz = construct_dc(&message);
 
-  amqp_bytes_t message_bytes;
+    amqp_bytes_t message_bytes;
 
-  message_bytes.len = sz;
-  message_bytes.bytes = message;
+    message_bytes.len = sz;
+    message_bytes.bytes = message;
 
-  for (i = 0; i < message_count; i++) {
-    uint64_t now = now_microseconds();
+    for (i = 0; i < message_count; i++) {
+        uint64_t now = now_microseconds();
 
-    die_on_error(amqp_basic_publish(conn,
-                                    1,
-                                    amqp_cstring_bytes("amq.direct"),
-                                    amqp_cstring_bytes(queue_name),
-                                    0,
-                                    0,
-                                    NULL,
-                                    message_bytes),
-                 "Publishing");
-    sent++;
-    if (now > next_summary_time) {
-      int countOverInterval = sent - previous_sent;
-      double intervalRate = countOverInterval / ((now - previous_report_time) / 1000000.0);
-      printf("%d ms: Sent %d - %d since last report (%d Hz)\n",
-             (int)(now - start_time) / 1000, sent, countOverInterval, (int) intervalRate);
+        die_on_error(amqp_basic_publish(conn,
+                                        1,
+                                        amqp_cstring_bytes("amq.direct"),
+                                        amqp_cstring_bytes(queue_name),
+                                        0,
+                                        0,
+                                        NULL,
+                                        message_bytes),
+                     "Publishing");
+        sent++;
+        if (now > next_summary_time) {
+            int countOverInterval = sent - previous_sent;
+            double intervalRate = countOverInterval / ((now - previous_report_time) / 1000000.0);
+            printf("%d ms: Sent %d - %d since last report (%d Hz)\n",
+                   (int)(now - start_time) / 1000, sent, countOverInterval, (int) intervalRate);
 
-      previous_sent = sent;
-      previous_report_time = now;
-      next_summary_time += SUMMARY_EVERY_US;
+            previous_sent = sent;
+            previous_report_time = now;
+            next_summary_time += SUMMARY_EVERY_US;
+        }
+
+        while (((i * 1000000.0) / (now - start_time)) > rate_limit) {
+            microsleep(2000);
+            now = now_microseconds();
+        }
     }
-
-    while (((i * 1000000.0) / (now - start_time)) > rate_limit) {
-      microsleep(2000);
-      now = now_microseconds();
-    }
-  }
 
     free(message);
 
-  {
-    uint64_t stop_time = now_microseconds();
-    int total_delta = stop_time - start_time;
+    {
+        uint64_t stop_time = now_microseconds();
+        int total_delta = stop_time - start_time;
 
-    printf("PRODUCER - Message count: %d\n", message_count);
-    printf("Total time, milliseconds: %d\n", total_delta / 1000);
-    printf("Overall messages-per-second: %g\n", (message_count / (total_delta / 1000000.0)));
-  }
+        printf("PRODUCER - Message count: %d\n", message_count);
+        printf("Total time, milliseconds: %d\n", total_delta / 1000);
+        printf("Overall messages-per-second: %g\n", (message_count / (total_delta / 1000000.0)));
+    }
 }
 
 int main(int argc, char const *const *argv)
 {
-  char const *hostname;
-  int port, status;
-  int rate_limit;
-  int message_count;
-  amqp_socket_t *socket = NULL;
-  amqp_connection_state_t conn;
+    char const *hostname;
+    int port, status;
+    int rate_limit;
+    int message_count;
+    amqp_socket_t *socket = NULL;
+    amqp_connection_state_t conn;
 
-  if (argc < 5) {
-    fprintf(stderr, "Usage: amqp_producer host port rate_limit message_count\n");
-    return 1;
-  }
+    if (argc < 5) {
+        fprintf(stderr, "Usage: amqp_producer host port rate_limit message_count\n");
+        return 1;
+    }
 
-  hostname = argv[1];
-  port = atoi(argv[2]);
-  rate_limit = atoi(argv[3]);
-  message_count = atoi(argv[4]);
+    hostname = argv[1];
+    port = atoi(argv[2]);
+    rate_limit = atoi(argv[3]);
+    message_count = atoi(argv[4]);
 
-  conn = amqp_new_connection();
+    conn = amqp_new_connection();
 
-  socket = amqp_tcp_socket_new(conn);
-  if (!socket) {
-    die("creating TCP socket");
-  }
+    socket = amqp_tcp_socket_new(conn);
+    if (!socket) {
+        die("creating TCP socket");
+    }
 
-  status = amqp_socket_open(socket, hostname, port);
-  if (status) {
-    die("opening TCP socket");
-  }
+    status = amqp_socket_open(socket, hostname, port);
+    if (status) {
+        die("opening TCP socket");
+    }
 
-  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
-                    "Logging in");
-  amqp_channel_open(conn, 1);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+    die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
+                      "Logging in");
+    amqp_channel_open(conn, 1);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
 
-  send_batch(conn, "test queue", rate_limit, message_count);
+    send_batch(conn, "test queue", rate_limit, message_count);
 
-  die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
-  die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
-  die_on_error(amqp_destroy_connection(conn), "Ending connection");
-  return 0;
+    die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+    die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+    die_on_error(amqp_destroy_connection(conn), "Ending connection");
+    return 0;
 }
 
